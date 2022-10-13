199aabedaa70d2b61471d8dc926bab0f85596413
Michael Huang <michaelh@casual.place>
2022-10-12 20:51:46 -0700

    Add collision check
    Made an event to check if crab entity collides with a block
    entity. Added a custom size to crab entity to easily change it.
    Placed temporary placeholders to notify when a collision happens
    and when the crab passes a gap.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use bevy::sprite::collide_aabb::collide;
 
 const WIN_WIDTH: f32 = 800.0;
 const WIN_HEIGHT: f32 = 500.0;
@@ -7,8 +8,11 @@ const GRAVITY: f32 = 10.0;
 const JUMP_ACCEL: f32 = -500.0;
 
 const BLOCK_WIDTH: f32 = 70.0;
+const BLOCK_SIZE: Vec2 = Vec2::new(BLOCK_WIDTH, WIN_HEIGHT);
 const BLOCK_SPEED: f32 = 256.0;
 const BLOCK_GAP: f32 = 140.0;
+
+const CRAB_SIZE: Vec2 = Vec2::new(32.0, 32.0);
 
 fn main() {
     App::new()
@@ -22,6 +26,7 @@ fn main() {
         .add_startup_system(add_sprites)
         .add_system(start_gravity)
         .add_system(start_block_move)
+        .add_system(check_collision)
         .run();
 }
 
@@ -42,7 +47,16 @@ fn add_sprites(mut commands: Commands, asset_server: Res<AssetServer>) {
     // crab entity (player)
     commands
         .spawn_bundle(SpriteBundle {
+            sprite: Sprite {
+                custom_size: Some(CRAB_SIZE),
+                ..default()
+            },
             transform: Transform::from_xyz(-(WIN_WIDTH / 2.0) + 100.0, 0.0, 0.0),
+            // currently the initial transform is baked into the entity. May want
+            // to change it to being set when the game starts. Perhaps in
+            // start_gravity. Since the different game states won't have the crab
+            // in the same place. Main menu could have it center, for example.
+            // same with the blocks.
             texture: asset_server.load("crab.png"),
             ..default()
         })
@@ -54,12 +68,12 @@ fn add_sprites(mut commands: Commands, asset_server: Res<AssetServer>) {
         .spawn_bundle(SpriteBundle {
             sprite: Sprite {
                 color: (Color::RED),
-                custom_size: Some(Vec2::new(BLOCK_WIDTH, WIN_HEIGHT)),
+                custom_size: Some(BLOCK_SIZE),
                 ..default()
             },
             transform: Transform::from_xyz(
                 WIN_WIDTH / 2.0,
-                // to get the top of the block lined up with the top of the window
+                // to account for gap between blocks
                 (WIN_HEIGHT / 2.0) + (BLOCK_GAP / 2.0),
                 0.0,
             ),
@@ -69,17 +83,16 @@ fn add_sprites(mut commands: Commands, asset_server: Res<AssetServer>) {
         .insert(Velocity { speed: 0.0 });
 
     // lower block entity (obstacle)
-
     commands
         .spawn_bundle(SpriteBundle {
             sprite: Sprite {
                 color: (Color::RED),
-                custom_size: Some(Vec2::new(BLOCK_WIDTH, WIN_HEIGHT)),
+                custom_size: Some(BLOCK_SIZE),
                 ..default()
             },
             transform: Transform::from_xyz(
                 WIN_WIDTH / 2.0,
-                // to get the top of the block lined up with the top of the window
+                // to account for gap between blocks
                 -WIN_HEIGHT / 2.0 - BLOCK_GAP / 2.0,
                 0.0,
             ),
@@ -111,6 +124,7 @@ fn start_gravity(
         // Put in limits to prevent going out of bounds.
         // Later, implement a loss when hitting the limit
         // Maybe only make the bottom bound a loss, upper bound can stay
+        // Or maybe both bounds can stay
         if transform.translation.y > WIN_HEIGHT / 2.0 {
             transform.translation.y = WIN_HEIGHT / 2.0;
         }
@@ -129,7 +143,43 @@ fn start_block_move(
         transform.translation.x -= vel.speed * time.delta_seconds();
 
         if transform.translation.x < (-WIN_WIDTH / 2.0) - (BLOCK_WIDTH / 2.0) {
-            transform.translation.x = (WIN_WIDTH / 2.0) + (BLOCK_WIDTH / 2.0)
+            transform.translation.x = (WIN_WIDTH / 2.0) + (BLOCK_WIDTH / 2.0);
+            println!("1 more!")
+
+            // the plan was to use a random number generator to randomly move the
+            // y transform of the blocks. The issue is that I would then need to
+            // reset the y value after every go around or the blocks may eventually
+            // go off screen. The issue is there are 2 blocks with different
+            // y transforms, so I can't just reset the y value.
+            // I could initially set the rng variable to 0 and subtract it from
+            // the y value. Then I set the variable equal to a new rng and add it
+            // to the y value. Therefore with each loop, it first resets the
+            // transform by whatever value it was and then gets a new value to
+            // change it after. That idea seems a little messy though.
+        }
+    }
+}
+
+fn check_collision(
+    crab_query: Query<(&Transform, &Sprite), With<Crab>>,
+    block_query: Query<(&Transform, &Sprite), With<Block>>,
+) {
+    let (crab_transform, crab_size) = crab_query.single();
+    // Consider looking into doing something about this unwrap.
+    // Maybe not since it should be guaranteed but idk.
+    let crab_size = crab_size.custom_size.unwrap();
+
+    for (block_transform, block_size) in block_query.iter() {
+        let block_size = block_size.custom_size.unwrap();
+        let collision = collide(
+            crab_transform.translation,
+            crab_size,
+            block_transform.translation,
+            block_size,
+        );
+        if let Some(_collision) = collision {
+            // will eventually make it change state to loss
+            println!("YOU LOSE");
         }
     }
 }
