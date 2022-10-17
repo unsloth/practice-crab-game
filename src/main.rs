7449a68adf92a58528d1f8846502abb40ed759bb
Michael Huang <michaelh@casual.place>
2022-10-17 03:00:40 -0700

    Add Menu and Replay Option
    Created a 'Menu' state for the menu screen. Made a function that
    starts the 'Play' state and a function that clears the screen
    by removing all entities except for the camera. Added those
    functions to the menu and game over screen.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use bevy::prelude::*;
 use bevy::sprite::collide_aabb::collide;
 
+const FONT_PATH: &str = "fonts/arcadeclassic/ARCADECLASSIC.TTF";
+
 const WIN_WIDTH: f32 = 800.0;
 const WIN_HEIGHT: f32 = 500.0;
 
@@ -23,8 +25,11 @@ fn main() {
             ..default()
         })
         .add_plugins(DefaultPlugins)
-        .add_state(GameState::Play)
+        .add_state(GameState::Menu)
         .add_startup_system(setup_camera)
+        .add_system_set(SystemSet::on_enter(GameState::Menu).with_system(setup_menu))
+        .add_system_set(SystemSet::on_update(GameState::Menu).with_system(start_play))
+        .add_system_set(SystemSet::on_exit(GameState::Menu).with_system(clean_menu))
         .add_system_set(SystemSet::on_enter(GameState::Play).with_system(add_sprites))
         .add_system_set(
             SystemSet::on_update(GameState::Play)
@@ -33,6 +38,8 @@ fn main() {
                 .with_system(check_collision),
         )
         .add_system_set(SystemSet::on_enter(GameState::Over).with_system(display_end_screen))
+        .add_system_set(SystemSet::on_update(GameState::Over).with_system(start_play))
+        .add_system_set(SystemSet::on_exit(GameState::Over).with_system(clean_menu))
         .run();
 }
 #[derive(Debug, Clone, Eq, PartialEq, Hash)]
@@ -40,6 +47,67 @@ enum GameState {
     Menu,
     Play,
     Over,
+}
+
+fn setup_menu(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn_bundle(
+        TextBundle::from_section(
+            "Crab Game",
+            TextStyle {
+                font: asset_server.load(FONT_PATH),
+                font_size: 100.0,
+                color: Color::WHITE,
+            },
+        )
+        .with_text_alignment(TextAlignment::CENTER)
+        .with_style(Style {
+            display: Display::Flex,
+            flex_direction: FlexDirection::Row,
+            align_self: AlignSelf::Center,
+            // janky way to horizontally center since 'justify_content' does nothing
+            position: UiRect {
+                left: Val::Px(WIN_WIDTH / 4.0),
+                ..default()
+            },
+            position_type: PositionType::Absolute,
+            ..default()
+        }),
+    );
+    commands.spawn_bundle(
+        TextBundle::from_section(
+            "Press Space to Play",
+            TextStyle {
+                font: asset_server.load(FONT_PATH),
+                font_size: 32.0,
+                color: Color::WHITE,
+            },
+        )
+        .with_text_alignment(TextAlignment::CENTER)
+        .with_style(Style {
+            display: Display::Flex,
+            flex_direction: FlexDirection::Row,
+            align_self: AlignSelf::FlexStart,
+            // janky way to horizontally center since 'justify_content' does nothing
+            position: UiRect {
+                left: Val::Px(WIN_WIDTH / 3.0),
+                ..default()
+            },
+            position_type: PositionType::Absolute,
+            ..default()
+        }),
+    );
+}
+
+fn start_play(mut state: ResMut<State<GameState>>, keys: Res<Input<KeyCode>>) {
+    if keys.just_pressed(KeyCode::Space) {
+        state.set(GameState::Play).unwrap();
+    }
+}
+
+fn clean_menu(mut commands: Commands, query: Query<Entity, Without<Camera>>) {
+    for entity in query.iter() {
+        commands.entity(entity).despawn_recursive();
+    }
 }
 
 #[derive(Component)]
@@ -196,7 +264,7 @@ fn display_end_screen(mut commands: Commands, asset_server: Res<AssetServer>) {
         TextBundle::from_section(
             "Game Over",
             TextStyle {
-                font: asset_server.load("fonts/arcadeclassic/ARCADECLASSIC.TTF"),
+                font: asset_server.load(FONT_PATH),
                 font_size: 100.0,
                 color: Color::WHITE,
             },
@@ -210,6 +278,29 @@ fn display_end_screen(mut commands: Commands, asset_server: Res<AssetServer>) {
             // janky way to horizontally center since 'justify_content' does nothing
             position: UiRect {
                 left: Val::Px(WIN_WIDTH / 4.0),
+                ..default()
+            },
+            position_type: PositionType::Absolute,
+            ..default()
+        }),
+    );
+    commands.spawn_bundle(
+        TextBundle::from_section(
+            "Press Space to Replay",
+            TextStyle {
+                font: asset_server.load(FONT_PATH),
+                font_size: 32.0,
+                color: Color::WHITE,
+            },
+        )
+        .with_text_alignment(TextAlignment::CENTER)
+        .with_style(Style {
+            display: Display::Flex,
+            flex_direction: FlexDirection::Row,
+            align_self: AlignSelf::FlexStart,
+            // janky way to horizontally center since 'justify_content' does nothing
+            position: UiRect {
+                left: Val::Px(WIN_WIDTH / 3.0),
                 ..default()
             },
             position_type: PositionType::Absolute,
